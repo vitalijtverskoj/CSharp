5d4f111feecb2ac634d37db89dcf9c9e73414ae3
vitalijtverskoj <vitalijtverskoj@gmail.com>
2024-01-20 09:39:13 +0300

    add task3 sem4:hw4

---
+++ Sem4/Hw4/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, 
+// второй – предпоследним и т.д.)
+
+void ArrayReverse(int[] arr)
+{
+    int num = 0;
+    for (int i = 0, j = arr.Length - 1; i < arr.Length / 2; i++, j--)
+    {
+        num = arr[i];
+        arr[i] = arr[j];
+        arr[j] = num;
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+Console.WriteLine($"Массив ДО: [{string.Join(", ", array)}]");
+ArrayReverse(array);
+Console.WriteLine($"Массив ПОСЛЕ: [{string.Join(", ", array)}]");

e35e43071e534ee201a85916bc0bf283e8961a14
vitalijtverskoj <vitalijtverskoj@gmail.com>
2024-01-23 22:56:37 +0300

    add Task1:Lesson04

---
+++ Lesson04/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] CreateMatrix(int rowsCount, int columnsCount)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);

36d0c071af4d18a7fa22a2ab2119f40f0912cb7e
vitalijtverskoj <vitalijtverskoj@gmail.com>
2024-02-05 23:49:33 +0300

    add Task2:homework7

---
+++ homework7/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine(Ackermann(1, 1));
